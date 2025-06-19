4fad1c7894b44ee681dc9186de98ff4f505c64d0
amlan564 <amlanroy4396@gmail.com>
2025-06-19 15:39:00 +0600

    admin logout added

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,7 +1,15 @@
 import { AlignJustify, LogOut } from "lucide-react";
 import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice";
 
 const AdminHeader = ({ setOpen }) => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b border-gray-200">
       <Button
@@ -13,7 +21,11 @@ const AdminHeader = ({ setOpen }) => {
         <span className="sr-only">Toggle Menu</span>
       </Button>
       <div className="flex flex-1 justify-end">
-        <Button className="bg-black text-white cursor-pointer" size="lg">
+        <Button
+          onClick={handleLogout}
+          className="bg-black text-white cursor-pointer"
+          size="lg"
+        >
           <LogOut />
           Logout
         </Button>
